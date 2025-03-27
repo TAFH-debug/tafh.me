f622293b6afe47f594b053dc4e4601fcc626769c
TAFH-debug <talim2007@mail.ru>
2025-03-27 21:11:12 +0500

    update metadata & logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import "./globals.css";
 import { Roboto } from 'next/font/google'
 import { Providers } from "./providers";
+import { Metadata } from "next";
  
 const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  title: "Talim Aushakhman",
+  description: "My personal site."
+}
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
